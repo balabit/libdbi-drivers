e24ef20dd50f800968933010e9a92344e05212e8
mhoenicka <mhoenicka>
2003-02-04 22:37:53 +0000

    fixes automake-autoconf-libtool incompatibility

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,11 @@
 # autogen.sh - generates configure using the autotools
 # $Id$
 aclocal
-automake --add-missing
-autoconf
+automake --add-missing -f
+
+## autoconf 2.53 will not work, at least on FreeBSD. Change the following
+## line appropriately to call autoconf 2.13 instead. This one works for
+## FreeBSD 4.7:
+autoconf213
+
 autoheader

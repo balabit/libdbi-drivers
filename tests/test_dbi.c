289874ade3dc131520c916d25a767293328d3246
mhoenicka <mhoenicka>
2008-01-01 00:16:50 +0000

    fixed isql call

--- tests/test_dbi.c
+++ tests/test_dbi.c
@@ -436,19 +436,19 @@ int init_db(struct CONNINFO* ptr_cinfo) {
   if (!strcmp(ptr_cinfo->drivername, "firebird")) {
     if (!strcmp(ptr_cinfo->hostname, "localhost")) {
       snprintf(command, 1024, 
-	       "echo \"CREATE DATABASE \'%s/%s\' user '%s' password '%s';\""
+	       "echo \"CREATE DATABASE \'%s/%s\';\""
 	       "| %s -e -pas %s "
 	       "-u %s -sql_dialect 3", ptr_cinfo->dbdir, 
-	       ptr_cinfo->dbname, ptr_cinfo->username, ptr_cinfo->password, 
+	       ptr_cinfo->dbname,  
 	       ( boolean ? "isql" : "isql-fb"), 
 	       ptr_cinfo->password, ptr_cinfo->username);
     }
     else { /* remote */
       snprintf(command, 1024, 
-	       "echo \"CREATE DATABASE \'%s:%s/%s\' user '%s' password '%s';\""
+	       "echo \"CREATE DATABASE \'%s:%s/%s\';\""
 	       "| %s -e -pas %s "
 	       "-u %s -sql_dialect 3", ptr_cinfo->hostname, ptr_cinfo->dbdir, 
-	       ptr_cinfo->dbname, ptr_cinfo->username, ptr_cinfo->password, 
+	       ptr_cinfo->dbname,  
 	       ( boolean ? "isql" : "isql-fb"), 
 	       ptr_cinfo->password, ptr_cinfo->username);
     }
